96e2a95141ead0a89cd21f62ce442c97d6f4793c
Ecricalitanoal <danila20041966@gmail.com>
2025-05-01 16:50:30 +0300

    Person class Javadoc

--- Java4/src/main/java/org/example/Models/Person.java
+++ Java4/src/main/java/org/example/Models/Person.java
@@ -2,17 +2,43 @@ package org.example.Models;
 
 import java.time.LocalDate;
 
+/**
+ * Класс, представляющий информацию о сотруднике и его отделе.
+ */
 public class Person {
 
-    //id;name;gender;BirtDate;Division;Salary
+    /** Уникальный идентификатор сотрудника */
     private final int id;
+
+    /** Полное имя сотрудника */
     private final String name;
+
+    /** Пол сотрудника */
     private final String gender;
+
+    /** Дата рождения сотрудника */
     private final LocalDate birtDate;
+
+    /** Подразделение, в котором работает сотрудник */
     private final Division division;
+
+    /** Заработная плата сотрудника */
     private final double salary;
 
-    public Person(int id, String name, String gender, LocalDate birthDate, Division division, double salary) {
+    /**
+     * Создает новый объект Person.
+     *
+     * @param id        уникальный идентификатор сотрудника
+     * @param name      полное имя сотрудника
+     * @param gender    пол сотрудника
+     * @param birthDate дата рождения
+     * @param division  подразделение компании
+     * @param salary    размер заработной платы
+     * @throws IllegalArgumentException если переданы некорректные параметры
+     */
+    //id;name;gender;BirtDate;Division;Salary
+    public Person(int id, String name, String gender, LocalDate birthDate,
+                  Division division, double salary) {
         this.id = id;
         this.name = name;
         this.gender = gender;
@@ -20,34 +46,69 @@ public class Person {
         this.division = division;
         this.salary = salary;
     }
+
+    /**
+     * Возвращает идентификатор сотрудника.
+     *
+     * @return числовой идентификатор
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Возвращает полное имя сотрудника.
+     *
+     * @return строка с именем
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Возвращает пол сотрудника.
+     *
+     * @return строка с указанием пола
+     */
     public String getGender() {
         return gender;
     }
 
+    /**
+     * Возвращает дату рождения сотрудника.
+     *
+     * @return объект LocalDate с датой рождения
+     */
     public LocalDate getBirthDate() {
         return birtDate;
     }
 
+    /**
+     * Возвращает подразделение сотрудника.
+     *
+     * @return объект Division
+     */
     public Division getDivision() {
         return division;
     }
 
+    /**
+     * Возвращает размер заработной платы.
+     *
+     * @return значение зарплаты
+     */
     public double getSalary() {
         return salary;
     }
 
-
+    /**
+     * Возвращает строковое представление объекта.
+     * @return строковое представление объекта
+     */
     @Override
     public String toString() {
-        return "Person{" + "id = " + id + ", name = '" + name + '\'' + ", gender = " + gender + ", division = " + division.toString() + ", salary = " + salary +
-                ", birthDate = " + birtDate + '}';
+        return "Person{" + "id = " + id + ", name = '" + name + '\'' +
+                ", gender = " + gender + ", division = " + division.toString() +
+                ", salary = " + salary + ", birthDate = " + birtDate + '}';
     }
 }
