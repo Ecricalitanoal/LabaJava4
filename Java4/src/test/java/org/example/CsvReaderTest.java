78a646c216430acd86d2178444118e399c5f2406
Ecricalitanoal <danila20041966@gmail.com>
2025-05-01 16:31:09 +0300

    unit test csvReader

---
+++ Java4/src/test/java/org/example/CsvReaderTest.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import org.example.FileReader.CsvReaderPerformer;
+import org.example.Models.Person;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CsvReaderTest {
+
+
+    @Test
+    void testValidFile() throws Exception {
+        CsvReaderPerformer readerService = new CsvReaderPerformer();
+        String validFile = "foreign_names.csv";
+        char separator = ';';
+
+        List<Person> personList = readerService.readCSVPersons(validFile, separator);
+
+        assertNotNull(personList);
+        assertFalse(personList.isEmpty());
+
+        Person firstInFile = personList.get(0);
+        //28281;Aahan;Male;15.05.1970;I;4800
+        assertEquals(28281, firstInFile.getId());
+        assertEquals("Aahan", firstInFile.getName());
+        assertEquals("Male", firstInFile.getGender());
+        assertEquals("I", firstInFile.getDepartment().getName());
+    }
+
+    @Test
+    void testInvalidFile() {
+        CsvReaderPerformer readerService = new CsvReaderPerformer();
+        String invalidFile = "notfound.csv";
+        char separator = ';';
+
+        List<Person> people = readerService.readCSVPersons(invalidFile, separator);
+        assertTrue(people.isEmpty());
+    }
+
+}
